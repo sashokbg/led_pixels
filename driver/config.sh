6c0b29d215694e47f5cde2ecac03bd264aa951f1
Alexander KIRILOV <alexander.n.kirilov@gmail.com>
2024-02-11 23:18:48 +0100

    added config.sh script

--- driver/config.sh
+++ driver/config.sh
@@ -1,10 +1,8 @@
-# Led Driver
-
-gcc main.c -o main -lX11
+#!/bin/sh
 
 groupadd plugdev
 
-sudo usermod -aG plugdev $USER
+usermod -aG plugdev $USER
 
 echo 'SUBSYSTEMS=="usb", ATTRS{idVendor}=="10c4", ATTRS{idProduct}=="ea60", GROUP="plugdev", MODE="0666"
 ' > /etc/udev/rules.d/40-dfuse.rules
